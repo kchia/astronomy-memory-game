66f7d941983166857e5d9c61f30043781d10930e
Jared Morgan <jared@majordanger.com>
2019-09-16 15:22:58 -0500

    added game board with temp layout and hamburger menu event with animation

--- scripts.js
+++ scripts.js
@@ -1,2 +1,28 @@
-const hamburger = document.getElementsByClassName('hamburger');
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('nav');
+
+// menu open/close event
+hamburger.addEventListener('click', openMenu);
+let navSwitch = 0;
+
+function openMenu(evt) {
+    console.log(hamburger[0]);
+    const bars = hamburger.children;
+    evt.preventDefault();
+    
+    if (navSwitch === 0) {
+        navigation.style.opacity = '1';
+        bars[0].classList.add('top');
+        bars[1].classList.add('middle');
+        bars[2].classList.add('bottom');
+        navSwitch = 1;
+    } else {
+        navigation.style.opacity = '0';
+        bars[0].classList.remove('top');
+        bars[1].classList.remove('middle');
+        bars[2].classList.remove('bottom');
+        navSwitch = 0;
+    }
+
+}
 
