ca0a4904e14539c6bb97f419f95262390e1f03b1
Jared Morgan <jared@majordanger.com>
2019-09-17 10:23:57 -0500

    ordered board loading to console when menu query selected

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('nav');
 
+// games objects > arrays > objects
+const games = {
+    planets: [
+        { name: 'Mercury', img: 'images/planets/mercury.png' },
+        { name: 'Venus', img: 'images/planets/venus.png' },
+        { name: 'Earth', img: 'images/planets/earth.png' },
+        { name: 'Mars', img: 'images/planets/mars.png' },
+        { name: 'Jupiter', img: 'images/planets/jupiter.png' },
+        { name: 'Saturn', img: 'images/planets/saturn.png' },
+        { name: 'Uranus', img: 'images/planets/uranus.png' },
+        { name: 'Neptune', img: 'images/planets/neptune.png' },
+        { name: 'Sun', img: 'images/planets/sun.png' },
+        { name: 'Moon', img: 'images/planets/moon.png' }
+    ],
+    // stars: ['Aries', 'Cancer', 'Capricorn', 'Cassiopeia', 'Centaurus', 'Gemini', 'Leo', 'Libra', 'Orion', 'Pegasus', 'Pisces', 'Sagittarius', 'Scorpius', 'Taurus', 'Ursa Major', 'Ursa Minor', 'Virgo']
+}
+
+// gameBoard array
+let gameBoard = [] ;
+
 // menu open/close event
 hamburger.addEventListener('click', openMenu);
 let navSwitch = 0;
 
 function openMenu(evt) {
-    console.log(hamburger[0]);
     const bars = hamburger.children;
     evt.preventDefault();
     
@@ -26,13 +45,29 @@ function openMenu(evt) {
 }
 
 // navigation menu item events
-// on hover
-navigation.addEventListener('mouseover', function(evt) {
+navigation.addEventListener('mouseover', function(evt) { // on hover
     evt.target.style.color = '#FDCB56';
 });
-// on exit hover
-navigation.addEventListener('mouseout', function(evt) {
+
+navigation.addEventListener('mouseout', function(evt) { // on exit hover
     evt.target.style.color = '#FFFCF1';
 });
 
-// on click load new board
+navigation.addEventListener('click', function(evt) { // on click load new board
+    evt.preventDefault();
+
+    const boardToLoad = navigation.querySelectorAll('a');
+    let arraySelector = evt.target.innerText;
+    // arraySelector = arraySelector.toLowerCase();
+    loadBoard(arraySelector.toLowerCase());
+});
+
+function loadBoard(arrayQuery) {
+    const board = games[arrayQuery];
+    for (let i = 0; i < 8; i += 2) {
+        let randomIndex = Math.floor(Math.random()*board.length);
+        console.log(board[randomIndex]);
+        console.log(board[randomIndex]);
+    }
+}
+
